f359b9114be8d0d8e36f287b7c0c771706b63592
git4harish4snow <128574118+git4harish4snow@users.noreply.github.com>
2023-03-22 12:24:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,8 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('Build ur own fruit smothiee')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index('Fruit')
+# Let's put a pick list here so they can pick the fruit they want to include 
+
 
 streamlit.dataframe(my_fruit_list)
