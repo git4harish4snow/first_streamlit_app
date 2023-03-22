bb7628659fce8376e1f734b3c62a2786073d1139
git4harish4snow <128574118+git4harish4snow@users.noreply.github.com>
2023-03-22 12:16:19 +0530

    Update streamlit_app.py
    added pandas and displayed data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas
 streamlit.title('my parents new healthy dinner')
 streamlit.header('breakfast menu')
 streamlit.text('	😄Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('Build ur own fruit smothiee')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
