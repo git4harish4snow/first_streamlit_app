599a6afcecfa69fe13eb6cba2ccee6f0523f41dc
git4harish4snow <128574118+git4harish4snow@users.noreply.github.com>
2023-03-25 16:00:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,5 @@ streamlit.dataframe(fruitvice_normalized)
 streamlit.header('fruitvice fruit advice')
 fruit_choice=streamlit.text_input('whta fruit u like to informed about','Kiwi')
 streamlit.write('user enterded',fruit_choice)
-#fruityvice_response=request.get("https://www.fruityvice.com/api/fruit"+fruit_choice)
+fruityvice_response=request.get("https://www.fruityvice.com/api/fruit"+fruit_choice)
 
