18c7d66a548a9d7aa4b4371fcadc1d8628db46e7
git4harish4snow <128574118+git4harish4snow@users.noreply.github.com>
2023-03-26 12:27:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,16 +36,16 @@ streamlit.dataframe(fruitvice_normalized)
 import snowflake.connector
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-my_cur.execute("select * from fruit_load_list")
+#my_cur.execute("select * from fruit_load_list")
 
 #my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+#streamlit.text("Hello from Snowflake:")
 
-my_data_row = my_cur.fetchone()
+#my_data_row = my_cur.fetchone()
 
 streamlit.text("fruit load list contains")
 
