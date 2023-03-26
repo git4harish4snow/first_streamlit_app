eabe7f8fc54f147f090ca5e745599f4fba870230
git4harish4snow <128574118+git4harish4snow@users.noreply.github.com>
2023-03-26 12:31:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 streamlit.title('my parents new healthy dinner')
+streamlit.text('Kale, Spinach & Rocket Smoothie@@@')
 streamlit.header('breakfast menu')
 #streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
