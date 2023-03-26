0c128af03e58f97090113bd854786b09ac866369
git4harish4snow <128574118+git4harish4snow@users.noreply.github.com>
2023-03-26 12:21:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ my_cur.execute("select * from fruit_load_list")
 streamlit.text("Hello from Snowflake:")
 my_data_row = my_cur.fetchone()
 streamlit.text=("fruit load list contains")
-streamlit.text(my_data_row)
+
 streamlit.text(my_data_row)
 
 
